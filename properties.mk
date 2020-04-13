de7b481e7fb8cd1a1cbdcc20cc4c50f6702ef419
nichcream <nichcream@codeaurora.org>
2020-04-13 15:45:43 +0530

    sdm660-common:  Set virtual displays to 0
    We don't have any virtual displays, however HWC assumes 1 virtual
    display by default. This was causing laggy display when allocating
    virtual displays, as done by surfaceflinger during screen recording
    for example.
    
    Signed-off-by: HeartStealer786 <beingme639@gmail.com>

--- properties.mk
+++ properties.mk
@@ -150,6 +150,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
+    debug.sdm.support_writeback=0 \
     ro.opengles.version=196610 \
     ro.vendor.display.cabl=0 \
     vendor.display.disable_skip_validate=1 \
