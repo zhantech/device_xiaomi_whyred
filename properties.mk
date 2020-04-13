47d54645e6bba23e640a12ad15a65db9caca488b
Shashank Baghel <theradcolor@gmail.com>
2020-04-13 15:59:44 +0530

    sdm660-common : enable pm.sleep_mode
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>
    
    Signed-off-by: HeartStealer786 <beingme639@gmail.com>

--- properties.mk
+++ properties.mk
@@ -229,6 +229,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
+#misc tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+    pm.sleep_mode=1
+
 # Time
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true
